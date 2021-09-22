3b6518cf6dbf4ffd8aae45340a757aa1c7bdf00f
Lim Tzeyi <limtzeyi@gmail.com>
2021-09-22 21:56:37 +0800

    added histogram for cumulative orders and deliveries

---
+++ js/A380_orders_deliveries_viz.js
@@ -0,0 +1,4 @@
+var vg_4 = "json/A380_orders_deliveries_viz.vg.json";
+vegaEmbed("#histogram3", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

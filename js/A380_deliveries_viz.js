7e8147fabde64017e3fa864e30786b1be4ecd13f
Lim Tzeyi <limtzeyi@gmail.com>
2021-09-22 21:00:30 +0800

    added viz for cumulative deliveries

--- js/A380_deliveries_viz.js
+++ js/A380_deliveries_viz.js
@@ -1,4 +1,4 @@
-var vg_2 = "json/A380_orders_viz.vg.json";
-vegaEmbed("#histogram1", vg_2).then(function(result) {
+var vg_3 = "json/A380_deliveries_viz.vg.json";
+vegaEmbed("#histogram2", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

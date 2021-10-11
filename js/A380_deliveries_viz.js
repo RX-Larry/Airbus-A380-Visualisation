3058f96278b53f6e9f8ea5de671aa84efa6146d7
Lim Tzeyi <limtzeyi@gmail.com>
2021-10-11 20:19:50 +0800

    added new vizzes

--- js/A380_deliveries_viz.js
+++
@@ -1,4 +0,0 @@
-var vg_3 = "json/A380_deliveries_viz.vg.json";
-vegaEmbed("#histogram2", vg_3).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
--- js/A380_orders_deliveries_viz.js
+++
@@ -1,4 +0,0 @@
-var vg_4 = "json/A380_orders_deliveries_viz.vg.json";
-vegaEmbed("#layered_histogram", vg_4).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
